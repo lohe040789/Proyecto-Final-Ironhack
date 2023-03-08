7499f9d6b1509b84ad4bbc135cc7940b17bdc916
lohe040789 <lohe_35@hotmail.com>
2023-03-08 17:50:58 +0100

    Cascara

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,29 +4,61 @@ import pandas as pd
 st.sidebar.title('Creado por ...')
 st.title('Proyecto Final') 
 st.title('Hagamos tu Menu')
-
-
 st.header('Necesitamos unos datos para poder ayudarte')
-left_column, right_column = st.columns(2)
-# You can use a column just like st.sidebar:
-left_column.button('Press me!')
-
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
-
-
 
 # Parametros de entrada:
 
 Genero = st.sidebar.selectbox('Genero?',('Hombre', 'Mujer'))
-Edad = st.sidebar.slider('Que edad tienes?', 0, 130, 25)
-Peso = st.sidebar.slider('Cuanto pesas?', 0, 130, 25)
-Estatura = st.sidebar.slider('Cuanto mides en "cm"?', 0, 130, 25)
+Tipo_de_actividad = st.sidebar.selectbox('Que tipo de actividad realizas?', ('Sedentario', 'Ligera', 'Moderada', 'Intensa'))
 Objetivo = st.sidebar.selectbox('Que objetivo tienes?', ('Perder peso', 'Ganar masa muscular', 'Mantenerse'))
 Horario_de_la_actividad = st.sidebar.selectbox('A que hora realizas actividad fisica?', ('Mañana', 'Tarde', 'Noche'))
-Tipo_de_actividad = st.sidebar.selectbox('Que tipo de actividad realizas?', ('Sedentario', 'Ligera', 'Moderada', 'Intensa'))
+Edad = st.sidebar.slider('Que edad tienes?', 10, 85, 25)
+Peso = st.sidebar.slider('Cuanto pesas?', 30, 150, 25)
+Estatura = st.sidebar.slider('Cuanto mides en "cm"?', 100, 200, 150)
 Tiempo_de_actividad = st.sidebar.slider('Cuantas horas al dia realizas actividad fisica?', 0, 4, 1)
+print(Genero, Edad, Peso, Estatura, Objetivo, Horario_de_la_actividad, Tipo_de_actividad, Tiempo_de_actividad)
+
+#Primero la información necesaria para realizar la formula
+def usuario():
+    edad = Edad
+    genero = Genero.lower()
+    peso = Peso
+    estatura = Estatura
+    if genero == 'hombre':
+        n1 = 66.5
+        p = 13.75 * peso
+        at = 5.003 * estatura
+        e = 6.77 * edad
+    elif genero == 'mujer':
+        n1 = 655.1
+        p = 9.56 * peso
+        at = 1.85 * estatura
+        e = 4.68 * edad
+
+    #GEB mujer= 665.1 + (9.56 x p) + (1.85 x cm) – (4.7 x anios) + actividad
+    #GEB hombre = 66.5 + (13.75 x p) + (5.003 x cm) - (6.77 x anios) + actividad
+    GEB_resultado = n1 + at + p - e
+
+    actividad_nivel = Tipo_de_actividad.lower()
+
+    if actividad_nivel == 'sedentario':
+        actividad_nivel = (30 * GEB_resultado /100) + GEB_resultado
+    elif actividad_nivel == 'ligera':
+        actividad_nivel = (50 * GEB_resultado /100) + GEB_resultado
+    elif actividad_nivel == 'moderada':
+        actividad_nivel = (75 * GEB_resultado /100) + GEB_resultado
+    elif actividad_nivel == 'intensa':
+        actividad_nivel = (100 * GEB_resultado /100) + GEB_resultado
+
+# Lo mas saludable a la hora de perder o ganar peso, se recomienda que sean 500kcal por dia. 
+    objetivo = Objetivo.lower()
+    if objetivo == 'perder peso':
+        kcal = actividad_nivel - 500
+    elif objetivo == 'mantenerse':
+        kcal = actividad_nivel
+    elif objetivo == 'ganar masa muscular':
+        kcal = actividad_nivel + 500 
+
+    return kcal
+    
+
